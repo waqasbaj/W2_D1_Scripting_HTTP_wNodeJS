07df375a277688a9afb0d7b4d5063a2c31fe8032
Waqas Bajwa <waqasbaj@gmail.com>
2018-01-22 16:23:11 +0000

    Printing with independant parameters

---
+++ Printing_with_parameters.js
@@ -0,0 +1,44 @@
+function getAndPrintHTML(options){
+
+
+  const https = require('https');
+
+
+
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - '/' is the root path, but if you wanted to read a
+// particular resource (like '/login/index.html'), that would be defined in the path
+var requestOptions = options;
+
+// notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+https.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  let responseData = '';
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length, ' \n');
+    responseData  +=data;
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    const parsedData = responseData.toString();
+    console.log('Response stream complete.');
+     console.log(parsedData);
+  });
+
+});
+
+}
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
